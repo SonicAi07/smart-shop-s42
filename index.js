c8767ec2462a9864da0ed04b3adaad88c3ea569a
SonicAi07 <sonicilyas077@gmail.com>
2023-01-15 10:54:08 +0530

    changed Origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const MailRouter = require('./routes/MailRouter')
 
 const app = express()
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://smart-shop-ilyas.netlify.app/",
     methods: ["GET", "POST"],
     credentials: true
 }))
